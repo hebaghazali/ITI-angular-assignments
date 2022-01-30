1d7d651848efc77d490cc8f837aed46821b4021d
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-01-30 14:42:19 +0200

    Get products from json-server database

---
+++ src/app/Services/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { IProduct } from 'src/app/ViewModels/iproduct';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this.httpClient.get<IProduct[]>(`${environment.APIURL}/products`);
+  }
+
+  getProductsByCatId(catId: number): Observable<IProduct[]> {
+    // if (category === '') return this.getProducts();
+
+    return this.httpClient.get<IProduct[]>(
+      `${environment.APIURL}/products?category-id=${Number(catId)}`
+    );
+  }
+
+  getProductById(id: number): Observable<IProduct> {
+    // return this.productList.find((product) => product.id === pId)!;
+    return this.httpClient.get<IProduct>(
+      `${environment.APIURL}/products/${id}`
+    );
+  }
+
+  // addProduct(product) {
+  //   this.productList.push(product);
+  // }
+
+  // updateProduct(id: number, name: string, quantity: number, price: number) {
+  //   const currentProduct = this.getProductById(id);
+  //   currentProduct.name = name;
+  //   currentProduct.quantity = quantity;
+  //   currentProduct.price = price;
+  // }
+
+  deleteProduct(id: number) {}
+
+  // getProductIDs(): number[] {
+  //   return this.productList.map((product) => product.id);
+  // }
+}
--- src/app/Services/static-products.service.ts
+++ src/app/Services/static-products.service.ts
@@ -8,64 +8,70 @@ export class StaticProductsService {
   private productList: IProduct[];
 
   constructor() {
-    this.productList = [
-      {
-        id: 874,
-        name: 'Apple Mac Mini ',
-        quantity: 3,
-        price: 1050,
-        img: 'https://m.media-amazon.com/images/I/71pcTYT+ICL._AC_SL1500_.jpg',
-        categoryID: 22,
-      },
-      {
-        id: 361,
-        name: 'HP ProDesk 600',
-        quantity: 1,
-        price: 240,
-        img: 'https://m.media-amazon.com/images/I/51rzf0w1I+S._AC_SL1144_.jpg',
-        categoryID: 22,
-      },
-      {
-        id: 362,
-        name: 'ADMI Gaming PC',
-        quantity: 5,
-        price: 875,
-        img: 'https://m.media-amazon.com/images/I/61sdqtqp0PL._AC_SL1000_.jpg',
-        categoryID: 22,
-      },
-      {
-        id: 516,
-        name: 'DELL VOSTRO',
-        quantity: 2,
-        price: 800,
-        img: 'https://m.media-amazon.com/images/I/91zKmiPmArS._AC_SL1500_.jpg',
-        categoryID: 30,
-      },
-      {
-        id: 485,
-        name: 'HP 250',
-        quantity: 4,
-        price: 750,
-        img: 'https://m.media-amazon.com/images/I/41HVQUl59YS._AC_.jpg',
-        categoryID: 30,
-      },
-      {
-        id: 545,
-        name: 'HP 15-dy2091wm',
-        quantity: 0,
-        price: 500,
-        img: 'https://m.media-amazon.com/images/I/414RM5sSaAL._AC_.jpg',
-        categoryID: 30,
-      },
-      {
-        id: 203,
-        name: 'Dell Inspiron 7506',
-        quantity: 1,
-        price: 500,
-        img: 'https://m.media-amazon.com/images/I/61hT2GQ93aL._AC_SL1200_.jpg',
-        categoryID: 30,
-      },
-    ];
+    // this.productList = [
+    //   {
+    //     id: 874,
+    //     title: 'Apple Mac Mini ',
+    //     quantity: 3,
+    //     price: 1050,
+    //     image:
+    //       'https://m.media-amazon.com/images/I/71pcTYT+ICL._AC_SL1500_.jpg',
+    //     'category-id': 22,
+    //   },
+    //   {
+    //     id: 361,
+    //     title: 'HP ProDesk 600',
+    //     quantity: 1,
+    //     price: 240,
+    //     image:
+    //       'https://m.media-amazon.com/images/I/51rzf0w1I+S._AC_SL1144_.jpg',
+    //     'category-id': 22,
+    //   },
+    //   {
+    //     id: 362,
+    //     title: 'ADMI Gaming PC',
+    //     quantity: 5,
+    //     price: 875,
+    //     image:
+    //       'https://m.media-amazon.com/images/I/61sdqtqp0PL._AC_SL1000_.jpg',
+    //     'category-id': 22,
+    //   },
+    //   {
+    //     id: 516,
+    //     title: 'DELL VOSTRO',
+    //     quantity: 2,
+    //     price: 800,
+    //     image:
+    //       'https://m.media-amazon.com/images/I/91zKmiPmArS._AC_SL1500_.jpg',
+    //     'category-id': 30,
+    //   },
+    //   {
+    //     id: 485,
+    //     title: 'HP 250',
+    //     quantity: 4,
+    //     price: 750,
+    //     image: 'https://m.media-amazon.com/images/I/41HVQUl59YS._AC_.jpg',
+    //     'category-id': 30,
+    //   },
+    //   {
+    //     id: 545,
+    //     title: 'HP 15-dy2091wm',
+    //     quantity: 0,
+    //     price: 500,
+    //     image: 'https://m.media-amazon.com/images/I/414RM5sSaAL._AC_.jpg',
+    //     'category-id': 30,
+    //   },
+    //   {
+    //     id: 203,
+    //     title: 'Dell Inspiron 7506',
+    //     quantity: 1,
+    //     price: 500,
+    //     image:
+    //       'https://m.media-amazon.com/images/I/61hT2GQ93aL._AC_SL1200_.jpg',
+    //     'category-id': 30,
+    //   },
+    // ];
+    this.productList = [];
   }
 
   getProducts(): IProduct[] {
@@ -75,7 +81,7 @@ export class StaticProductsService {
   getProductsByCatId(cId: number): IProduct[] {
     if (Number(cId) === 0) return this.productList;
     return this.productList.filter(
-      (product) => product.categoryID === Number(cId)
+      (product) => product['category-id'] === Number(cId)
     );
   }
 
@@ -87,9 +93,9 @@ export class StaticProductsService {
     this.productList.push(product);
   }
 
-  editProduct(id: number, name: string, quantity: number, price: number) {
+  editProduct(id: number, title: string, quantity: number, price: number) {
     const currentProduct = this.getProductById(id);
-    currentProduct.name = name;
+    currentProduct.title = title;
     currentProduct.quantity = quantity;
     currentProduct.price = price;
   }
