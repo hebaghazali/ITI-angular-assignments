82b55bb1cb0851683efcb14d52ac32c53c47254f
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-02-04 23:16:51 +0200

    Add validation and use 1 component for the form instead of 2

--- src/app/Services/products.service.ts
+++ src/app/Services/products.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
-import { catchError, Observable, retry, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IProduct } from 'src/app/ViewModels/iproduct';
 import { ICategory } from 'src/app/ViewModels/icategory';
 
@@ -13,21 +9,21 @@ import { ICategory } from 'src/app/ViewModels/icategory';
   providedIn: 'root',
 })
 export class ProductsService {
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error(error.error));
-  }
+  // private handleError(error: HttpErrorResponse) {
+  //   if (error.status === 0) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong.
+  //     console.error(
+  //       `Backend returned code ${error.status}, body was: `,
+  //       error.error
+  //     );
+  //   }
+  //   // Return an observable with a user-facing error message.
+  //   return throwError(() => new Error(error.error));
+  // }
 
   httpOption: {
     headers: HttpHeaders;
